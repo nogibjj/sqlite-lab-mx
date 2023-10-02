c2e1491525823ee1ca6638937221ecab66bfbcfe
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-10-01 23:52:04 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -12,13 +12,28 @@ def test_add():
     
     assert add(1, 2) == 3
 
-def test_query():
+def test_query(): #add where condition
     result = subprocess.run( [
             "python",
             "main.py",
             "general_query",
-            "SELECT * FROM  WHERE airline = 'Alaska Airlines'",
+            "SELECT * FROM GroceryDB.db ,
         ],
         capture_output=True,
         text=True,
         check=True,)
+   assert add(1, 2) == 3
+
+def test_read_data():
+    """tests read_data"""
+    result = subprocess.run(
+        ["python", "main.py", "read_data"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert add(1,2) == 3
+if __name__ =="__main__":
+    test_add()
+    test_read_data()
+    test_query()

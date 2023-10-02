258e0a3929b9bd484844c5db18f057c4fcbff551
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-10-01 23:48:38 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,9 +2,23 @@
 Test goes here
 
 """
-
+import subprocess
 from mylib.calculator import add
 
 
 def test_add():
+    result = subprocess.run(["python","main.py","extract"],
+                           capture_output = True), text = True, check = True,)
+    
     assert add(1, 2) == 3
+
+def test_query():
+    result = subprocess.run( [
+            "python",
+            "main.py",
+            "general_query",
+            "SELECT * FROM  WHERE airline = 'Alaska Airlines'",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,)

67f08d12360209964185e0f28fd5a0aa2754f624
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-10-01 23:29:34 -0400

    Update query.py

--- mylib/query.py
+++ mylib/query.py
@@ -2,15 +2,37 @@
 
 import sqlite3
 
+#LOG_FILE = "query_log.md"
 
-def query():
+#def log_query(query):
+
+def query(query):
     """Query the database for the top 5 rows of the GroceryDB table"""
     conn = sqlite3.connect("GroceryDB.db")
+    
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
-    print("Top 5 rows of the GroceryDB table:")
-    print(cursor.fetchall())
+    
+    cursor.execute(query)
+    #"SELECT * FROM GroceryDB"
+    if (query.strip().lower().startwith("insert")
+        or query.strip().lower().startwith("update")
+        or query.strip().lower().startwith("delete"):
+            conn.commit()
+    cursor.close()
     conn.close()
-    return "Success"
 
+    #log_query(f"{query}")
+    
+    #print("Top 5 rows of the GroceryDB table:")
+    #print(cursor.fetchall())
+#def create():
+ #   conn = sqlite3.connect("GroceryDB.db")
+  #  c=conn.cursor()
+   # c.execute()
 
+def read_data():
+    conn=sqlite3.connect("GroceryDB.db")
+    c = conn.cursor()
+    c.execute("select * from GroceryDB.db limit 5")
+    data = c.fetchall()
+    return data
